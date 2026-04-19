39acd13d33ad13ee0979625987116aa6958f3ab0
imam-programmer <imamprogrammer09@gmail.com>
2026-04-19 07:04:01 +0600

    finaly gallery done

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ let container=document.querySelector('.container')
 let img=document.querySelectorAll('img')
 let big_div=document.querySelector('.big-div img')
 let big=document.querySelector('.big-div')
+let btn=document.createElement('button');
+let body=document.querySelector('body')
+btn.innerHTML='<i class="fa-solid fa-angle-left"></i>'
+big.prepend(btn)
+big.style.textAlign='center'
 
+btn.addEventListener('click',()=>{
+   big.classList.remove('imag')
+})
 img.forEach((i)=>{
 i.addEventListener('click',()=>{
 big_div.src=i.src
-big.classList.toggle('imag')
+big.classList.add('imag')
 })
 })
